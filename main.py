1735474d7326e864eb20c973542beb3716066ada
BostonSlettum22 <816661@mystma.org>
2021-01-20 16:47:22 +0000

    echo 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def echo(x,y):
+    bazinga = x
+
+    for i in range(1 ,y):
+        bazinga = bazinga + x
+        
+
+    return bazinga
+
+print(echo("hello", 13))
+
